f5299d6d6966f943c8fe202f60bf827951c0316c
wesley <wesleycherfi@hotmail.fr>
2018-08-23 03:43:04 +0200

    Initial commit

---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: 'ey-up3fxfKRuBUApG2SonMbo0Ky570n0',
+  domain: 'ygg.eu.auth0.com',
+  callbackUrl: 'http://127.0.0.1:8080/callback',
+  apiUrl: 'YOUR_API_IDENTIFIER'
+}
